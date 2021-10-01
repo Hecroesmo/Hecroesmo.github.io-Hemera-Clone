a897aaffc8869ef3ec5efb38f75231d7d7216994
Hecroesmo <hecroesmo.jesus77@outlook.com>
2021-10-01 22:06:06 +0100

    change to external js

---
+++ js/draw-symbol.js
@@ -0,0 +1,10 @@
+/** draw the logo symbol */
+
+var canvas = document.getElementById('symbol')
+var ctx = canvas.getContext('2d')
+
+ctx.fillStyle = '#fff'
+ctx.fillRect(120, 0, 53, 65)
+ctx.fillRect(120, 85, 53, 65)
+ctx.fillRect(30, 40, 53, 65)
+ctx.fillRect(210, 40, 53, 65)
