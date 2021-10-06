dab69cbcd6b8ed46cf1187f468e51881d06e9a80
Hecroesmo <hecroesmo.jesus77@outlook.com>
2021-10-06 22:42:53 +0100

    finished-responsive-header-content

--- js/draw-symbol.js
+++ js/draw-symbol.js
@@ -2,9 +2,11 @@
 
 var canvas = document.getElementById('symbol')
 var ctx = canvas.getContext('2d')
-
 ctx.fillStyle = '#fff'
-ctx.fillRect(120, 0, 53, 65)
-ctx.fillRect(120, 85, 53, 65)
-ctx.fillRect(30, 40, 53, 65)
-ctx.fillRect(210, 40, 53, 65)
+const width = 55
+const height = 67
+
+ctx.fillRect(120, 0, width, height)
+ctx.fillRect(120, 85, width, height)
+ctx.fillRect(30, 40, width, height)
+ctx.fillRect(210, 40, width, height)
